a8d78c179449d4d24aca3a8d8b84d5c4c3283b66
anupam <roya55324@gmail.com>
2024-11-22 01:43:06 +0600

    Introduces a new feature.

---
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import React from 'react';
+import { SignIn } from '@clerk/nextjs';
+
+type Props = {};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const SignInPage = (props: Props) => {
+    return <SignIn />;
+};
+
+export default SignInPage;
---
+++ app/dashboard/[workspaceld]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+    // authintication
+    // if account dos'n exit
+    return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
